e519b7f8b3b7cfb176f4eaec3ad90b0620c663f7
Rodrigo Barros <rodrigobarros2802@gmail.com>
2023-07-08 19:51:54 -0300

    nesse commit iniciei adicionando o addEventListener mouse move

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener ('mousemove', function(e) {
+    const bg = document.querySelector('.bg');
+    const bird = document.querySelector('.bird');
+    const content = document.querySelector('.content');
+
+    bg.style.width = 100 + e.pageX/100 + '%';
+    bg.style.height = 100 + e.pageX/100 + '%';
+
+
+})
